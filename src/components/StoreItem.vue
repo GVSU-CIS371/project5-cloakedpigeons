4264c02edf3033cc277b2163540dc0932b24a9fc
BreannaaZ <breannazinky@gmail.com>
2024-04-08 15:39:00 -0400

    Added Home.vue and StoreItem.vue

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -1,7 +1,25 @@
 <template>
   <!-- your answer -->
+  <v-card>
+    <v-card-title>{{ product.data.name }}</v-card-title>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-chip>{{ product.data.price }}</v-chip>
+      <v-rating 
+      v-model="product.data.rating" 
+      readonly
+      half-increments
+      ></v-rating>
+    </v-card-actions>
+    <v-img :src="product.data.image" aspect-ratio="1"></v-img>
+    <v-card-text>{{ product.data.description }}</v-card-text>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
 // your answer
+import { ProductDoc } from "../types/product";
+import { defineProps } from 'vue';
+const { product } = defineProps<{ product: ProductDoc }>();
+
 </script>
